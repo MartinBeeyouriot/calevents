50d5db3558d7800e4132e2743df43caed1964dbc
Martin <martinbeeyouriot@gmail.com>
2021-02-08 09:34:18 +0800

    Setting up emberjs routes.
    New routes.

---
+++ calevents-client/app/routes/events.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { queryManager } from 'ember-apollo-client';
+import query from 'calevents-client/gql/queries/users.graphql';
+
+// eslint-disable-next-line ember/no-classic-classes
+export default Route.extend({
+  apollo: queryManager(),
+
+  model(params) {
+    let variables = { id: params.id };
+    return this.apollo.watchQuery({ query, variables }, 'users');
+  },
+});
